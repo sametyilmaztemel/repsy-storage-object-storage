40f47e5fbe5a172d7bf043c12422a9a4f74a5d59
Samet Yilmaz Temel <sametyilmaztemel@users.noreply.github.com>
2025-05-08 13:28:25 +0300

    feat: add StorageStrategy module

--- src/main/java/com/repsy/storage/objectstorage/StorageStrategy.java
+++ src/main/java/com/repsy/storage/objectstorage/StorageStrategy.java
@@ -1,3 +1,13 @@
 package com.repsy.storage.objectstorage;
 
 import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageStrategy {
+    void init();
+    String store(MultipartFile file);
+    byte[] load(String filename);
+    void delete(String filename);
+    boolean exists(String packageName, String version, String fileName);
+    void store(String packageName, String version, String fileName, byte[] fileContent);
+    byte[] load(String packageName, String version, String fileName);
+}
